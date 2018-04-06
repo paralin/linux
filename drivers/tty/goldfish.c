c3a18d166c312d71593d12eaa19101c0b35da2b6
Roman Kiryanov <rkir@google.com>
2018-04-06 15:52:14 -0700

    Add missing include to drivers/tty/goldfish.c
    Change-Id: I4a71a1cad108c8b3bb3875e8483ac2ac7357268c
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- drivers/tty/goldfish.c
+++ drivers/tty/goldfish.c
@@ -18,6 +18,7 @@
 #include <linux/mm.h>
 #include <linux/dma-mapping.h>
 #include <linux/serial_core.h>
+#include <linux/of.h>
 
 /* Goldfish tty register's offsets */
 #define	GOLDFISH_TTY_REG_BYTES_READY	0x04
