206cca13d6c91b29f977cf0cf2c47b6387ac1dbc
David Arcari <darcari@redhat.com>
2018-08-01 12:13:49 -0400

    [x86] mark whiskey-lake processor supported
    Message-id: <1533125629-12870-1-git-send-email-darcari@redhat.com>
    Patchwork-id: 225809
    O-Subject: [ BZ 1609604] mark whiskey-lake processor supported
    Bugzilla: 1609604
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1609604
    Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=17474338
    Upstream Status: RHEL Only
    Tested: Successful excecution of platform-test suite.
    
    Whiskey Lake is cpu model 142 with stepping 11.  Stepping > 11 for cpu model
    142 is not supported.
    
    For model 158, the highest supported stepping is 10.
    
    Cc: Prarit Bhargava <prarit@redhat.com>
    Cc: David Arcari <darcari@redhat.com>
    Cc: Dave Young <dyoung@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -845,19 +845,24 @@ static bool valid_intel_processor(__u8 family, __u8 model, __u8 stepping)
 	case INTEL_FAM6_HASWELL_L:
 	case INTEL_FAM6_HASWELL_X:
 
-	case INTEL_FAM6_KABYLAKE:
-	case INTEL_FAM6_KABYLAKE_L:
-
 	case INTEL_FAM6_XEON_PHI_KNM:
 	case INTEL_FAM6_XEON_PHI_KNL:
 		valid = true;
+		break;
+
+	case INTEL_FAM6_KABYLAKE:
+		valid = (stepping <= 10);
+		break;
+
+	case INTEL_FAM6_KABYLAKE_L:
+		valid = (stepping <= 11);
 		break;
 
 	case INTEL_FAM6_SKYLAKE_L:
 	case INTEL_FAM6_SKYLAKE:
 	case INTEL_FAM6_SKYLAKE_X:
 		/* stepping > 4 is Cascade Lake and is not supported */
-		valid = (boot_cpu_data.x86_stepping <= 4);
+		valid = (stepping <= 4);
 		break;
 
 	default:
