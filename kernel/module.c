cc744530a9d420fdb8923b7c14c727cafb40cffb
Laura Abbott <labbott@redhat.com>
2019-01-07 21:03:10 +0000

    modules: add rhelversion MODULE_INFO tag
    There were several changes in the 5.0-rc1 kernel that required
    this patch to be rebased with some context diff.
    
        Patchwork-id: 7379
        O-Subject: [kernel team] [RHEL8.0 BZ 1544999 1/2] modules: add rhelversion MODULE_INFO tag
        Bugzilla: 1544999
        RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
        RH-Acked-by: Oleg Nesterov <oleg@redhat.com>
        RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
        Bugzilla: http://bugzilla.redhat.com/1544999
    
        kernel-alt commit 9816b4b6ca5d4c0504d8806ce99442c90afbc8e6
        Author: Prarit Bhargava <prarit@redhat.com>
        Date:   Fri Feb 9 18:09:26 2018 -0500
    
            [kernel] modules: add rhelversion MODULE_INFO tag
    
            Message-id: <20180209180926.5955-3-prarit@redhat.com>
            Patchwork-id: 205587
            O-Subject: [RHEL-ALT-7.5 BZ 1542796 2/2] modules: add rhelversion MODULE_INFO tag
            Bugzilla: 1542796
            RH-Acked-by: David Arcari <darcari@redhat.com>
            RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
            RH-Acked-by: Steve Best <sbest@redhat.com>
    
            Bugzilla: http://bugzilla.redhat.com/1542796
    
            rhel7 commit 9af8c3992ce5fff253c49abc0dedb3ec77badba4
            Author: Kyle McMartin <kmcmarti@redhat.com>
            Date:   Fri Sep 12 17:46:12 2014 -0400
    
                [kernel] modules: add rhelversion MODULE_INFO tag
    
                Message-id: <20140912174612.GS29420@redacted.bos.redhat.com>
                Patchwork-id: 94052
                O-Subject: [RHEL7.1 PATCH 2/6 v4] modules: add rhelversion MODULE_INFO tag
                Bugzilla: 1110315
                RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
                RH-Acked-by: Johannes Weiner <jweiner@redhat.com>
                RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
                From: Kyle McMartin <kmcmarti@redhat.com>
    
                Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1110315
    
                >From kylem's RHEL8 post:
    
                Let's not lose this ability in RHEL8. Sadly can't be applied to RHEL7
                because it breaks KABI due to the struct module change.
    
                Inspired by bits of a147e4729d9920ccae2c717ca65c811cc0f93a1d in RHEL6,
                which got lost for RHEL7.0. :/
    
                Kyle is correct and doing this would break RHEL7's KABI.  I'm taking this
                patch in as a base and the next patch will resolve the KABI issues.
    
                [v2]: dzickus, cleanup add_rhelversion()
                [v4]: resubmitted along with 16k stacks change
    
                Signed-off-by: Jarod Wilson <jarod@redhat.com>
    
            Cc: Richard Guy Briggs <rgb@redhat.com>
            Cc: Peter Rajnoha <prajnoha@redhat.com>
            Cc: Laura Abbott <labbott@redhat.com>
            Cc: Josh Poimboeuf <jpoimboe@redhat.com>
            Signed-off-by: Herton R. Krzesinski <herton@redhat.com>
    
        Signed-off-by: Herton R. Krzesinski <herton@redhat.com>
    Acked-by: Don Zickus <dzickus@redhat.com>
    Acked-by: Prarit Bhargava <prarit@redhat.com>

--- kernel/module.c
+++ kernel/module.c
@@ -796,6 +796,7 @@ static struct module_attribute modinfo_##field = {                    \
 
 MODINFO_ATTR(version);
 MODINFO_ATTR(srcversion);
+MODINFO_ATTR(rhelversion);
 
 static char last_unloaded_module[MODULE_NAME_LEN+1];
 
@@ -1260,6 +1261,7 @@ static struct module_attribute *modinfo_attrs[] = {
 	&module_uevent,
 	&modinfo_version,
 	&modinfo_srcversion,
+	&modinfo_rhelversion,
 	&modinfo_initstate,
 	&modinfo_coresize,
 	&modinfo_initsize,
