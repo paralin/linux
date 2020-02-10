295222a656a710a3292b503207702c2d1ddc2e69
Luca Weiss <luca@z3ntu.xyz>
2020-02-10 23:40:18 +0100

    sgm3140: add 'flash off' timer + cleanup

--- drivers/leds/leds-sgm3140.c
+++ drivers/leds/leds-sgm3140.c
@@ -16,6 +16,8 @@ struct sgm3140 {
 
 	struct led_classdev_flash fled_cdev;
 	struct v4l2_flash *v4l2_flash;
+
+	struct timer_list powerdown_timer;
 };
 
 static struct sgm3140 *flcdev_to_sgm3140(struct led_classdev_flash *flcdev)
@@ -30,23 +32,19 @@ int sgm3140_strobe_set(struct led_classdev_flash *fled_cdev, bool state)
 	if (state) {
 		gpiod_set_value_cansleep(priv->flash_gpio, 1);
 		gpiod_set_value_cansleep(priv->enable_gpio, 1);
+		mod_timer(&priv->powerdown_timer,
+			  jiffies + msecs_to_jiffies(250));
 	} else {
 		gpiod_set_value_cansleep(priv->enable_gpio, 0);
 		gpiod_set_value_cansleep(priv->flash_gpio, 0);
+		del_timer_sync(&priv->powerdown_timer);
 	}
 
 	return 0;
 }
 
-//static int sgm3140_timeout_set(struct led_classdev_flash *fled_cdev,
-//			       u32 timeout)
-//{
-//	return 0;
-//}
-
 struct led_flash_ops sgm3140_flash_ops = {
 	.strobe_set = sgm3140_strobe_set,
-//	.timeout_set = sgm3140_timeout_set,
 };
 
 int sgm3140_brightness_set(struct led_classdev *led_cdev,
@@ -55,12 +53,20 @@ int sgm3140_brightness_set(struct led_classdev *led_cdev,
 	struct led_classdev_flash *fled_cdev = lcdev_to_flcdev(led_cdev);
 	struct sgm3140 *priv = flcdev_to_sgm3140(fled_cdev);
 
-	if (brightness == LED_OFF)
+	if (brightness == LED_OFF) {
 		gpiod_set_value_cansleep(priv->enable_gpio, 0);
-	else
+	} else {
 		gpiod_set_value_cansleep(priv->enable_gpio, 1);
+	}
 
 	return 0;
+}
+
+static void sgm3140_powerdown_timer(struct timer_list *t)
+{
+	struct sgm3140 *priv = from_timer(priv, t, powerdown_timer);
+
+	gpiod_set_value_cansleep(priv->enable_gpio, 0);
 }
 
 #if IS_ENABLED(CONFIG_V4L2_FLASH_LED_CLASS)
@@ -70,7 +76,7 @@ static void sgm3140_init_v4l2_flash_config(struct sgm3140 *priv,
 	struct led_classdev *led_cdev = &priv->fled_cdev.led_cdev;
 	struct led_flash_setting *s;
 
-	strlcpy(v4l2_sd_cfg->dev_name, led_cdev->name,
+	strlcpy(v4l2_sd_cfg->dev_name, led_cdev->dev->kobj.name,
 		sizeof(v4l2_sd_cfg->dev_name));
 
 	s = &v4l2_sd_cfg->intensity;
@@ -78,8 +84,6 @@ static void sgm3140_init_v4l2_flash_config(struct sgm3140 *priv,
 	s->max = 1;
 	s->step = 1;
 	s->val = 1;
-
-	//v4l2_sd_cfg->has_external_strobe = led_cfg->has_external_strobe;
 }
 
 #else
@@ -88,9 +92,6 @@ static void sgm3140_init_v4l2_flash_config(struct sgm3140 *priv,
 {
 }
 #endif
-//static const struct v4l2_flash_ops v4l2_flash_ops = {
-//	.external_strobe_set = max77693_led_external_strobe_set,
-//};
 
 static int sgm3140_probe(struct platform_device *pdev)
 {
@@ -130,10 +131,13 @@ static int sgm3140_probe(struct platform_device *pdev)
 		return -EINVAL;
 	}
 
+	timer_setup(&priv->powerdown_timer, sgm3140_powerdown_timer, 0);
+
 	fled_cdev = &priv->fled_cdev;
 	led_cdev = &fled_cdev->led_cdev;
 
 	fled_cdev->ops = &sgm3140_flash_ops;
+	// TODO ?
 //	fled_cdev->timeout.max = 300000; /* 300ms */
 
 	led_cdev->brightness_set_blocking = sgm3140_brightness_set;
@@ -142,7 +146,6 @@ static int sgm3140_probe(struct platform_device *pdev)
 
 	init_data.fwnode = of_fwnode_handle(child_node);
 	init_data.devicename = SGM3140_NAME;
-	init_data.default_label = NULL;
 
 	platform_set_drvdata(pdev, priv);
 
@@ -158,16 +161,12 @@ static int sgm3140_probe(struct platform_device *pdev)
 
 	/* Create V4L2 Flash subdev */
 	priv->v4l2_flash = v4l2_flash_init(&pdev->dev, of_fwnode_handle(child_node),
-					   fled_cdev, /*&v4l2_flash_ops*/ NULL,
+					   fled_cdev, NULL,
 					   &v4l2_sd_cfg);
 	if (IS_ERR(priv->v4l2_flash)) {
 		ret = PTR_ERR(priv->v4l2_flash);
 		goto err_v4l2_flash_init;
 	}
-
-	of_node_put(child_node);
-
-	dev_err(&pdev->dev, "sgm3140 registered successfully!\n");
 
 	return 0;
 
@@ -181,6 +180,8 @@ err_flash_register:
 static int sgm3140_remove(struct platform_device *pdev)
 {
 	struct sgm3140 *priv = platform_get_drvdata(pdev);
+
+	del_timer_sync(&priv->powerdown_timer);
 
 	v4l2_flash_release(priv->v4l2_flash);
 	led_classdev_flash_unregister(&priv->fled_cdev);
