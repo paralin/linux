22591d8107a9a916e698a03b65f7bc28d55a2234
Justin M. Forbes <jforbes@fedoraproject.org>
2021-02-19 13:27:26 -0600

    Changes for building stable Fedora
    There are a couple of changes to things to make sure that our patches
    are built against Greg's linux-5.11.y branch, that we generate the
    specfile correctly, and that we do not version based on gitrev.  To
    test changes you can run:
    
    make IS_FEDORA=1 DIST=".fc34" BUILDID="" BUILD=300 RHDISTGIT_BRANCH=f34 dist-git-test
    
    Signed-off-by: Justin M. Forbes <jforbes@fedoraproject.org>

--- redhat/genspec.sh
+++ redhat/genspec.sh
@@ -21,7 +21,8 @@ LAST_MARKER=${14}
 SINGLE_TARBALL=${15}
 TARFILE_RELEASE=${16}
 SNAPSHOT=${17}
-BUILDID=${18}
+UPSTREAM=${18}
+BUILDID=${19}
 RPMVERSION=${KVERSION}.${KPATCHLEVEL}.${KSUBLEVEL}
 clogf="$SOURCES/changelog"
 # hide [redhat] entries from changelog
@@ -48,7 +49,7 @@ if [[ -z $lasttag ]]; then
 fi
 echo "Gathering new log entries since $lasttag"
 git log --topo-order --reverse --no-merges -z --format="- %s (%an)%n%b" \
-	^master "$lasttag".. -- ':!/redhat/rhdocs' | ${0%/*}/genlog.py >> "$clogf"
+	^$UPSTREAM "$lasttag".. -- ':!/redhat/rhdocs' | ${0%/*}/genlog.py >> "$clogf"
 
 grep -v "tagging $RPM_VERSION" "$clogf" > "$clogf.stripped"
 cp "$clogf.stripped" "$clogf"
@@ -167,7 +168,7 @@ ARK_COMMIT_URL="https://gitlab.com/cki-project/kernel-ark/-/commit"
 #
 # May need to preserve word splitting in EXCLUDE_FILES
 # shellcheck disable=SC2086
-git log --no-merges --pretty=oneline --no-decorate master.. $EXCLUDE_FILES | \
+git log --no-merges --pretty=oneline --no-decorate $UPSTREAM.. $EXCLUDE_FILES | \
 	sed "s!^\([^ ]*\)!$ARK_COMMIT_URL/\1\n &!; s!\$!\n!" \
 	> "$SOURCES"/Patchlist.changelog
 
