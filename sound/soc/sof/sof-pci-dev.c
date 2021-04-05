a6ee48ae6a520dc0a8cf265f1ad83a2e458009f1
Mark Pearson <markpearson@lenovo.com>
2021-04-05 10:12:28 -0400

    This is a backport of https://github.com/thesofproject/linux/pull/2768
    This is needed to fix a rare problem where the SOF audio driver
    hangs during a reboot. It occurs about once in 30 reboots and is
    hard to reproduce but has been seen on the X1 Carbon Gen9.
    
    The backport wasn't clean. I backported the following commits from
    the https://github.com/thesofproject/linux repository
    578778fd0c847b966679802dcb357b9d3acb44c8
    8f31938cd1990138941d519cc571506070e4d9a0
    a40cbfd2b54ea6d4698d2351b46fa0e958bf6220
    0b3afc4b486b573c2e60eaf5684435a5d0b91828
    31627609bbf60163317832644a241f6dc1f3e9b6
    59737eaebfcc10cbb9f20166977185007bfd6da9
    
    The last commit was extra and needed to add shutdown functionality.
    I implemented the change in penultimate commit in sof-pci-dev.c
    instead of pci-tgl.c as that file doesn't exist yet in this version
    of kernel. I wasn't sure if it was better to do a small surgical
    change, or to add in more commits - let me know if I need to tackle
    this differently
    
    First time doing a backport to the Fedora kernel - let me know if
    there are any procedure or implementation preferences or errors

--- sound/soc/sof/sof-pci-dev.c
+++ sound/soc/sof/sof-pci-dev.c
@@ -269,7 +269,7 @@ static const struct sof_dev_desc ehl_desc = {
 	.default_tplg_path = "intel/sof-tplg",
 	.default_fw_filename = "sof-ehl.ri",
 	.nocodec_tplg_filename = "sof-ehl-nocodec.tplg",
-	.ops = &sof_cnl_ops,
+	.ops = &sof_tgl_ops,
 };
 #endif
 
